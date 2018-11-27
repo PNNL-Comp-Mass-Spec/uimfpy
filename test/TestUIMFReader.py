2d58e5a3fcddafab483ca27b539786db59f8bc38
coldfire79 <joonyong.lee@pnnl.gov>
2018-11-26 17:52:10 -0800

    Update TestUIMFReader.py
    Add an unit test to test the LZF decompression

--- test/TestUIMFReader.py
+++ test/TestUIMFReader.py
@@ -3,6 +3,8 @@ python -m unittest test.TestUIMFReader
 '''
 
 import unittest
+import re
+from pyteomics import mzml,auxiliary
 from uimfpy.UIMFReader import *
 
 class TestUIMFReader(unittest.TestCase):
@@ -11,6 +13,13 @@ class TestUIMFReader(unittest.TestCase):
         print("Create UIMFReader")
         self.reader = UIMFReader('test/test.uimf')
 
+    def inspect_mzML_file(self, fpath):
+        spectra = []
+        with open(fpath, 'rb') as f:
+            for obj in mzml.read(f):
+                spectra.append(obj)
+        return spectra
+    
     def test_calibration_params_by_frame(self):
         print("The average TOF length:", self.reader.calibration_params_by_frame[1])
         self.assertEqual(self.reader.calibration_params_by_frame[1],
@@ -39,8 +48,37 @@ class TestUIMFReader(unittest.TestCase):
         for i in mz_peaks:
             total += mz_peaks[i]['mz'].shape[0]
         print("The number of peaks in the first frame:", total)
-        print(mz_peaks[i])
 
+    def test_decompress(self):
+        spectra = self.inspect_mzML_file('test/test.mzML')
+        mz_peaks = self.reader.get_mz_peaks(1)
+
+        for spectrum in spectra:
+            spectrum_int = spectrum['intensity array']
+            spectrum_mz = spectrum['m/z array']
+            
+            if (len(spectrum_int) == 0):
+                continue
+            
+            nonzero_idx = np.nonzero(spectrum_int)[0]
+            num_nonzeros = nonzero_idx.shape[0]
+            
+            info = spectrum['id']  # frame=1 scan=16 frameType=1
+            frame_num = int(re.search('frame=(\d+)', info).group(1))
+            scan_num = int(re.search('scan=(\d+)', info).group(1))
+            
+            if (frame_num,scan_num) in mz_peaks:
+                peaks = mz_peaks[(frame_num,scan_num)]
+                assert num_nonzeros == peaks['mz'].shape[0], print('number of intensities different')
+                k = 0
+                for mz, intensity in zip(peaks['mz'], peaks['int']):
+                    assert spectrum_int[nonzero_idx[k]] == intensity, print('intensity different')
+                    assert abs(spectrum_mz[nonzero_idx[k]] - mz)<0.000001, print('mz different')
+                    k += 1
+            else:
+                print(frame_num, scan_num, "not found")
+            
+        print('Testing decompress: all passed')
 
 if __name__ == '__main__':
     unittest.main()
